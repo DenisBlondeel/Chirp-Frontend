b8ba553c8436907957a9a4b40220cc0e2cd0f052
denis blondeel <denisblondeel@hotmail.com>
2020-12-08 13:40:03 +0100

    angular material not working fix

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ChirpFrontend
+# SocialMedia
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.7.
 
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/chirp-frontend'),
+      dir: require('path').join(__dirname, './coverage/social-media'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
