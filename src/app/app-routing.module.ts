8497533763271c55401a9173dfba5fd4028c16be
denis blondeel <denisblondeel@hotmail.com>
2020-12-15 15:07:12 +0100

    added authentication to frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { PostsComponent } from './component/posts/posts.component';
 
 const routes: Routes = [
-  { path: '', component: PostsComponent },
+  { path: '', component: PostsComponent, canActivate: [AuthGuard] },
   /*{ path: 'heroes', component: HeroesComponent},
   { path: 'dashboard', component: DashboardComponent},
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
