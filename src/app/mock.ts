fa3d5112f18a90ee3745f501b50cb214d160858d
denis blondeel <denisblondeel@hotmail.com>
2021-01-05 14:24:21 +0100

    added rest lookup for posts

--- src/app/mock.ts
+++
@@ -1,8 +0,0 @@
-import { Post } from './model/post';
-
-export const MOCKDATA: Post[] =
-[
-    {text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"},
-    {text: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."},
-    {text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
-]
