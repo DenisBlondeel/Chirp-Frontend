b236b8b5e8596958b5e3586bfd8dbdd7923df0df
denis blondeel <denisblondeel@hotmail.com>
2021-01-08 18:05:21 +0100

    added rest call to add users after reg

---
+++ src/app/service/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
+import { HttpClient } from "@angular/common/http";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private httpUserCall: string = "http://localhost:9090/user"
+
+  constructor(private http: HttpClient) { }
+
+  addUser(newUser: User): Observable<any>{
+    console.log("new user " + newUser)
+    return this.http.post(this.httpUserCall, newUser)
+  }
+
+
+}
